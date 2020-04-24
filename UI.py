718e3b14c572e333c4e20c07868cccd061030fba
Yaropolk <scalesrzn@gmail.com>
2020-04-25 00:19:15 +0300

    Many parse word

--- UI.py
+++ UI.py
@@ -32,47 +32,88 @@ class Interface(Frame):
 
         # Забираем значение инпута 
         def inputValue():
-            value = Keyword.get()
+            sParse = Keyword.get()
+            sDomain = DomainAnalys.get()
             TextLog.delete(1.0, END)
-            TextLog.insert(1.0,"Вы ввели: " + value + '\nИ выбрали поисковую систему ' + DropDown.get())
-            return value
+            TextLog.insert(1.0,"\nВы ввели имя домена: " + sDomain + '\nИ выбрали поисковую систему ' + DropDown.get())
+            TextLog.insert(1.0,"Вы ввели ключевую фразу: " + sParse)
+            aValue = []
+            aValue.append(sParse)
+            aValue.append(sDomain)
+
+            return aValue
 
         #Получаем ответ от сервера в виде HTML документа
         def getValueButton():
-            val = function.createRequest(str(inputValue()),DropDown.get())
-            print("Сгенерированный HTTP запрос: " + val)
-            response = function.getPage(val,headers)
-            TextLog.insert(1.0,"Ответ " + response)
-            if DropDown.get() == 'Yandex':
-                xPathResponseYandex(response)
-            else:
-                xPathResponseGoogle(response)
+            sIntVal = re.split(r",", inputValue()[0])
+            for sInputValue in sIntVal: 
+                val = function.createRequest(str(sInputValue),DropDown.get(),"Parse")
+                print("Сгенерированный HTTP запрос: " + val)
+                response = function.getPage(val,headers)
+                TextLog.insert(1.0,"Ответ " + response)
+                if DropDown.get() == 'Yandex':
+                    xPathResponseYandex(response)
+                else:
+                    xPathResponseGoogle(response)
 
+        #Получаем результаты анализа от сервера
+        def getDomainValue():
+            sIntVal = re.split(r",", inputValue()[1])
+            for sInputValue in sIntVal: 
+                val = function.createRequest(str(sInputValue),DropDown.get(),"Analys")
+                print("Сгенерированный HTTP запрос: " + val)
+                response = function.getPage(val,headers)
+                TextLog.insert(1.0,"Ответ " + response)
+                if DropDown.get() == 'Yandex':
+                    xPathResponseYandex(response)
+                else:
+                    xPathResponseGoogle(response)
+       
         def xPathResponseGoogle(response):
             print('Арбайтен')
 
         def xPathResponseYandex(response):
             #Разделяем полученную строку на список и убираем []
             aClearResponse = []
+            aDirtResponse = []
             aResponse = re.split(r",\[",re.findall(r'\[\".*\d\]', response)[0])
             print(len(aResponse))
             for i in range(len(aResponse)):
-                aClearResponse.append(re.sub(r'[\]\[]','',aResponse[i])) 
+                aDirtResponse.append(re.sub(r'[\]\[]','',aResponse[i]))
+                aClearResponse.append(re.sub(r'[\]\[]','',aDirtResponse[i])) 
                 TextResult.insert(1.0, re.sub(r'[\]\[]','',aClearResponse[i]) + '\n')
-                print(aClearResponse)
-        
+                # print(aClearResponse)
+            return aClearResponse
         # # Задаем путь для сохранения файла
         # def directory():   
-  
-        def saveExcel():
+        def saveExcelParse():
             sFilePath = fd.asksaveasfilename(filetypes=( ("Excel files", "*.xls"),
                                                         ("All files", "*.*") ))
             #Проверяем на пустоту путь сохранения файлы и если не пусто, то выполняем сохранение
             if sFilePath != '':
                 aClearResponse = []
                 aDirtResponse = []
-                val = function.createRequest(str(inputValue()),DropDown.get())
-                response = function.getPage(val,headers)
+                aAllKeyResponse =[]
+                sIntVal = re.split(r",", inputValue()[0])
+                for sInputValue in sIntVal: 
+                    val = function.createRequest(str(sInputValue),DropDown.get(),"Parse")
+                    response = function.getPage(val,headers)
+                    aClearResponse = xPathResponseYandex(response)
+                    for i in aClearResponse:
+                        aAllKeyResponse.append(i)
+                function.createEXCEL(aAllKeyResponse,sFilePath)
+                # print(aClearResponse)
+        
+        def saveExcelAnalys():
+            sFilePath = fd.asksaveasfilename(filetypes=( ("Excel files", "*.xls"),
+                                                        ("All files", "*.*") ))
+            #Проверяем на пустоту путь сохранения файлы и если не пусто, то выполняем сохранение
+            if sFilePath != '':
+                aClearResponse = []
+                aDirtResponse = []
+                val = function.createRequest(str(inputValue()[1]),DropDown.get(), "Analys")
+                #response = function.getPage(val,headers)
+                response = TextResult.get(1.0, END)
                 aResponse = re.split(r",\[",re.findall(r'\[\".*\d\]', response)[0])
                 for i in range(len(aResponse)):
                     aDirtResponse.append(re.sub(r'[\]\[]','',aResponse[i]))
@@ -80,7 +121,6 @@ class Interface(Frame):
                 # вызов функции создания EXCEL
                 function.createEXCEL(aClearResponse,sFilePath)
                 print(aClearResponse)
-
         #Получаем значение из выпадающег осписка
         # value = (listbox.get(listbox.curselection()))
 
@@ -99,12 +139,9 @@ class Interface(Frame):
         self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y)) 
         
 
-
- 
-
         # Располагаем объекты на форме
         # Объявляем все элементы:
-        KeywordLabel = Label(text="Введите ключевое слово:")
+        KeywordLabel = Label(text="Введите ключевое слово или слова через запятую:")
         DropDownLabel = Label(text = 'Выберите поисковую систему:')
         Keyword = Entry()
         DropDown = Combobox(self.parent)
@@ -113,29 +150,39 @@ class Interface(Frame):
         TextLog = Text(width=50, height=20)
         TextResult = Text(width=50, height=20)
         SaveValue = Button(text = "Сохранить параметры парсинга", command = inputValue )
-        SaveExcel = Button(text = 'Сохранить в EXCEL документ', command = saveExcel )
+        SaveExcel = Button(text = 'Сохранить в EXCEL документ', command = saveExcelParse )
+        
+        #Интерфейсные объекты для анализа
+        DomainAnalysLabel = Label(text="Введите название домена для анализа:")
+        DomainAnalys = Entry()
+        Analys  = Button(text = 'Начать анализ', command = getDomainValue)
+        AnalysSave = Button(text = 'Сохранить в EXCEL документ результаты анализа', command = saveExcelAnalys )
        
         # NameSpace
         DropDown['values'] = ('Yandex', 'Google')  
         DropDown.current(1)  # вариант по-умолчанию
 
         # Размещаем элементы
-        KeywordLabel.grid(row = 0, column = 2, sticky="w")
-        Keyword.grid(row = 0,column = 3)
-        DropDownLabel.grid(row = 1, column = 0)
-        # self.DropDown.grid(row = 1, column = 1)
-        SaveValue.grid(row = 3, column = 1)
-        SaveExcel.grid(row = 5,column = 1)
-        Parse.grid(row = 4, column = 1)
+
+        #0-й столбец
         TextLog.grid(row = 0, column = 0)
+        KeywordLabel.grid(row = 1, column = 0, sticky="w")
+        Keyword.grid(row = 2,column = 0)
+        DomainAnalysLabel.grid(row = 3, column = 0, sticky="w")
+        DomainAnalys.grid(row = 4,column = 0)
+        DropDownLabel.grid(row = 5, column = 0)
+        DropDown.grid(row=6, column=0) 
+        SaveValue.grid(row = 7, column = 0)
+
+        #1-й столбец
         TextResult.grid(row = 0, column = 1)
-        DropDown.grid(column=0, row=3) 
-        Close.grid(row = 5,column = 3 )
-
-
-
-        
-        
+        Parse.grid(row = 1, column = 1)
+        SaveExcel.grid(row = 2,column = 1)
+        Analys.grid(row = 6, column = 1)
+        AnalysSave.grid(row = 7, column = 1)
+       
+        #2-й столбец
+        Close.grid(row = 7,column = 3 )
 
         # Забираем с инпутов значения
         
--- function.py
+++ function.py
@@ -21,11 +21,17 @@ def getPage(url,headers):
     print(htmlcode)
     return htmlcode
 
-def createRequest(word,url):
-    if url == 'Yandex':
-        requestUrl = 'https://www.bukvarix.com/keywords/?q=' + word
-    else:
-        requestUrl = 'https://keywordtool.io/ru/search/keywords/google/391766?category=web&keyword=' + word +'/ru/search/keywords/google/391766?category=web&amp;keyword=%D0%9A%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D0%BE%D0%B5%20%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&amp;__cf_chl_jschl_tk__=76f1c5c2a038afc8dbe6d12d91bf16c88e03f744-1585445735-0-ARQj6MSDaZqGT-weUgW0VA0VUjp2AfFiV8CYa63-emLJMyyfhK6Ten6QTtwqh3V22HpaNKe6HMZ-6oyjFfurJ8meCE41WpeuTz79hnCruoYbsTXfd8HoLtBSa7YGJWwQDkEGur2foPd1LItRgsq7RsrYI9lKn6QIoJPllkY0tURO5jW7vLg2DpTPh4XbDzmrMkMSC2zwSzHOPSkfOlfd1-1s5b8Qnb4Pyx04ZwAchfYEtnpIId-CWLvLeY0enHMmrzTi_M2VvH1PhwKM2_bcQ5Bxx4I3eUc9DdMCwpgQLwTZcjWquPEf1l1zVHKps1DwT3M7ZSSz0NK6cVv1A3L2mleVxTi1NXNT2ot_1uNc_SMrN8wC7rXWuitmsDNAg_eZ1ldw6_iRsTKd-bcF5TV5kNmG_dIvxB3d3c2S5hEB7CYEluJWCN4WEbFZKvfgSMv8C96fFZS2ACFpZadpSDoUguZ7tIF0ghXi-xLQjwa066nZZztiSUZMAxEQnuxrWCaryg" method="POST" enctype="application/x-www-form-urlencoded"'
+def createRequest(word,url,type):
+    if type == "Parse":
+        if url == 'Yandex':
+            requestUrl = 'https://www.bukvarix.com/keywords/?q=' + word
+        else:
+            requestUrl = 'https://keywordtool.io/ru/search/keywords/google/391766?category=web&keyword=' + word +'/ru/search/keywords/google/391766?category=web&amp;keyword=%D0%9A%D0%BB%D1%8E%D1%87%D0%B5%D0%B2%D0%BE%D0%B5%20%D1%81%D0%BB%D0%BE%D0%B2%D0%BE&amp;__cf_chl_jschl_tk__=76f1c5c2a038afc8dbe6d12d91bf16c88e03f744-1585445735-0-ARQj6MSDaZqGT-weUgW0VA0VUjp2AfFiV8CYa63-emLJMyyfhK6Ten6QTtwqh3V22HpaNKe6HMZ-6oyjFfurJ8meCE41WpeuTz79hnCruoYbsTXfd8HoLtBSa7YGJWwQDkEGur2foPd1LItRgsq7RsrYI9lKn6QIoJPllkY0tURO5jW7vLg2DpTPh4XbDzmrMkMSC2zwSzHOPSkfOlfd1-1s5b8Qnb4Pyx04ZwAchfYEtnpIId-CWLvLeY0enHMmrzTi_M2VvH1PhwKM2_bcQ5Bxx4I3eUc9DdMCwpgQLwTZcjWquPEf1l1zVHKps1DwT3M7ZSSz0NK6cVv1A3L2mleVxTi1NXNT2ot_1uNc_SMrN8wC7rXWuitmsDNAg_eZ1ldw6_iRsTKd-bcF5TV5kNmG_dIvxB3d3c2S5hEB7CYEluJWCN4WEbFZKvfgSMv8C96fFZS2ACFpZadpSDoUguZ7tIF0ghXi-xLQjwa066nZZztiSUZMAxEQnuxrWCaryg" method="POST" enctype="application/x-www-form-urlencoded"'
+    if type == "Analys":
+        if url == 'Yandex':
+            requestUrl = 'https://www.bukvarix.com/site/?q=' + word + '&region=msk&v=table'
+        else:
+            requestUrl = 'https://www.bukvarix.com/site/?q=' + word + '&region=gmsk&v=table'
     return requestUrl
 
 def createEXCEL(aKeywords,filename):
@@ -58,5 +64,5 @@ def createEXCEL(aKeywords,filename):
         ws.write(i+1,2,countchar,style)
         ws.write(i+1,3,countworld,style)
         ws.write(i+1,4,counthardworld,style)
-    wb.save(filename)
+    wb.save(filename+".xls")
     win32api.MessageBox(0, 'Успешно сохранено в %s '%(filename), 'Успех!')
